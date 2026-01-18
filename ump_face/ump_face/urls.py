4e3985dee581d1d42b52c94d1364c44a61781fd0
elmrabet salah eddine <zakaria.agoulmami24@ump.ac.ma>
2026-01-18 11:19:34 +0100

    version avant finle

--- ump_face/ump_face/urls.py
+++ ump_face/ump_face/urls.py
@@ -44,7 +44,6 @@ urlpatterns = [
     path('friends/add/<int:user_id>/', views.add_friend, name='add_friend'),  
 
     # Supprimer des amis (protégée)
-    path('friends/remove/<int:user_id>/', views.remove_friend, name='remove_friend'),  
     
     # Recherche d'utilisateurs (protégée)
     path('search/', views.search_users, name='search_users'),  
